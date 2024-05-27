eed16baae1f05b4a55d24d20c15aba6ef7808d1f
Roisfaozi <roisfaozi55@gmail.com>
2024-05-27 14:22:32 +0700

    Feat: Update attribute product route parameter create authorization at attribute product route

--- apps/server/internal/routers/attributeprod.router.go
+++ apps/server/internal/routers/attributeprod.router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/Roisfaozi/black-coffee-collaborations/internal/handlers"
+	"github.com/Roisfaozi/black-coffee-collaborations/internal/middleware"
 	"github.com/Roisfaozi/black-coffee-collaborations/internal/repository"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
@@ -13,8 +14,8 @@ func attributeprod(g *gin.Engine, d *sqlx.DB) {
 	repo := repository.NewAtrributeProd(d)
 	handler := handlers.NewAttributeProd(repo)
 
-	router.GET("/delivery", handler.GetAllDelivery)
-	router.GET("/delivery/:id", handler.GetDeliveryProduct)
-	router.GET("/size", handler.GetAllSize)
+	router.GET("/delivery", middleware.Authjwt("admin", "user"), handler.GetAllDelivery)
+	router.GET("/delivery/:product_id", middleware.Authjwt("admin", "user"), handler.GetDeliveryProduct)
+	router.GET("/size", middleware.Authjwt("admin", "user"), handler.GetAllSize)
 
 }
