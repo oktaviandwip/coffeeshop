ef21e9a78126565b03209ca702358ac83932434f
guslizikri <zikrizg7@gmail.com>
2024-05-29 09:31:16 +0700

    integration cart

--- apps/client/src/components/OrderItem.jsx
+++ apps/client/src/components/OrderItem.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import ProductBanner from '../assets/product-image.png';
 
-export default function OrderItem(image, title, price, size, count) {
+export default function OrderItem({ image, title, price, size, quantity }) {
   return (
     <div className="flex justify-between p-5 font-rubik text-black items-center">
       <div className="flex items-center gap-x-2">
-        <img src={ProductBanner} alt="" className="w-[88px] h-[88px] rounded-xl" />
+        <img src={image} alt="" className="w-[88px] h-[88px] rounded-xl" />
         <div className="text-base">
-          <p>Coffee Latte</p>
-          <p>2</p>
-          <p>Regular</p>
+          <p>{title}</p>
+          <p>{quantity}</p>
+          <p>{size}</p>
         </div>
       </div>
-      <p className="">IDR 39.000</p>
+      <p className="">{price}</p>
     </div>
   );
 }
